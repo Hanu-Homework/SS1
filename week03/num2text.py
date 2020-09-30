ad9ed14b900a4edc9d5bc75782ff0a9881ee97cf
Tang Ba Minh <tangbamiinh@gmail.com>
2020-09-30 09:18:22 +0700

    Changed to another version

--- week03/num2text.py
+++ week03/num2text.py
@@ -1,3 +1,41 @@
+num_text = {
+    0: "Không",
+    1: "Một",
+    2: "Hai",
+    3: "Ba",
+    4: "Bốn",
+    5: "Năm",
+    6: "Sáu",
+    7: "Bảy",
+    8: "Tám",
+    9: "Chín",
+}
+
+
+def num_to_text_less_than_1000(num: int) -> str:
+    global num_text
+
+    don_vi = num % 10
+    num //= 10
+
+    chuc = num % 10
+    num //= 10
+
+    tram = num % 10
+
+    ret = ""
+    if tram > 0:
+        ret += num_text[tram] + " Trăm "
+    if chuc > 0:
+        if chuc == 1:
+            ret += "Mười "
+        else:
+            ret += num_text[chuc] + " Mươi "
+    if don_vi > 0:
+        ret += num_text[don_vi] + " "
+    return ret
+
+
 def num2text(num: int) -> str:
     """
     Translate a number into VND text format
@@ -16,66 +54,48 @@ def num2text(num: int) -> str:
         9: "Chín",
     }
 
-    suffixes = [
+    suffixes = (
         "Đồng",
-        "Mươi",
-        "Trăm",
         "Ngàn",
-        "Mươi",
-        "Trăm",
-        "Triệu",
-        "Mươi",
-        "Trăm",
-        "Tỷ",
-        "Mươi",
-        "Trăm",
-        "Ngàn",
-        "Mươi",
-        "Trăm",
         "Triệu",
         "Tỷ",
-    ]
+        "Ngàn",
+        "Triệu",
+        "Tỷ"
+    )
 
-    l = len(str(num))
-
+    parts = []
     clone = num
-    count = 0
-    result = ""
-
-    is_thousand_suffix_read = False
 
     while clone > 0:
-        num_value = clone % 10
+        parts.append(clone % 1000)
+        clone //= 1000
 
-        if num_value == 0:
-            count += 1
-            clone //= 10
-            continue
+    parts.reverse()
 
-        suffix = count % len(suffixes)
+    ret = ""
 
-        if suffix % 3 == 0:
-            is_thousand_suffix_read = True
-        else:
-            is_thousand_suffix_read = False
+    for index, part in enumerate(parts):
 
-        result = num_text[num_value] + " " + \
-            suffixes[suffix] + " " + result
-
-        count += 1
-        clone //= 10
-
-    if not is_thousand_suffix_read:
-        result += suffixes[count - count % 3] + " "
+        ret += num_to_text_less_than_1000(part) + \
+            suffixes[len(parts) - 1 - index] + " "
 
     if num % 10 == 0:
-        result += "Chẵn"
+        ret += "Chẵn"
 
-    return result
+    return ret
 
 
 if __name__ == "__main__":
-    num = int(input())
-    print(f'{num:,}')
-    result = num2text(num)
-    print(result)
+    test_cases = [
+        100_100_100,
+        100_111_100,
+        9_860_100_100,
+        345_134_156,
+        124_194_342
+    ]
+
+    for test_case in test_cases:
+        print(f'{test_case:,}')
+        print(num2text(test_case))
+        print()
